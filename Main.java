7b46623b338dfecd7771475244656e15c6562824
Thomas Gardner <80729894+Lava4creeper@users.noreply.github.com>
2022-05-23 01:36:06 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+//import statements
+
+//functions go here
+
+//************ Main Routine ***********
+
+//set up dictionaries / lists needed to hold data
+
+//Ask user if they have used the program before & show instructions if necessary
+
+//Loop to get ticket details
+
+  //get name (can't be blank)
+
+  //get age (between 1 and 130)
+
+  //calculate ticket price
+
+  //loop to ask for snacks
+
+  //calculate snack price
+
+  //ask for payment method (and apply surchanrge if needed)
+
+//calculate total sales and profit
+
+//output data to text file
+
