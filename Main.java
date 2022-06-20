589aa7ed98fadfdb4483d2a1e6cb9100bdff6224
Thomas Gardner <80729894+Lava4creeper@users.noreply.github.com>
2022-06-20 01:48:37 +0000

    added a number checker component

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 //import statements
 import java.util.Scanner;
+import java.util.InputMismatchException;
 //functions go here
 class Main{
   //get name (can't be blank)
-  public static String getName() {
+  public static String getName(String EXITCODE) {
     String name = "";
     Boolean validName = false;
     Scanner getName = new Scanner(System.in);
-    while (validName != true) {      
+    while ((validName != true) & (name.equals(EXITCODE) == false)) {      
       System.out.println("What is your name?");
       name = getName.nextLine();
       if (name != "") {
@@ -20,6 +21,31 @@ class Main{
     }
     return name;
   } 
+//get age (between 1 and 130)
+  public static int getAge() {
+    Scanner age = new Scanner(System.in);
+    String error = "Please enter a valid integer between 12 and 130";
+    boolean valid = false;
+    int inputAge = 0;
+    do {
+      try {
+         System.out.println("What is your age?");
+         inputAge = age.nextInt();
+         if(inputAge < 12 || inputAge > 130){
+           System.out.println(error);
+           }
+         else {
+           valid = true;
+           }
+      }
+       catch (InputMismatchException e) {
+         System.out.println(error);
+         inputAge = 0;
+         age.next();
+         }
+       } while (!valid);
+    return inputAge;
+   }
   //************ Main Routine ***********
   public static void main(String[] args) {
     //set up dictionaries / lists needed to hold data
@@ -27,6 +53,7 @@ class Main{
     String inputName = "";
     int count = 0;
     int MAX_TICKETS = 5;
+    int inputAge = 0;
     //Ask user if they have used the program before & show instructions if necessary
     //Loop to get ticket details
     while ((count < MAX_TICKETS) & (inputName.equals(EXITCODE) == false)){
@@ -37,7 +64,9 @@ class Main{
         System.out.println("1 ticket remaining");
       }
       //get name (can't be blank)
-      inputName = getName();
+      inputName = getName(EXITCODE);
+      inputAge = getAge();
+      System.out.println("Your name is " + inputName + ", you are " + inputAge + " years old");
       count ++;     
       }
     if (count == MAX_TICKETS) {
@@ -48,7 +77,6 @@ class Main{
       System.out.println(count + " tickets sold. \n" + (MAX_TICKETS - count) + " tickets still available");
     }
     }
-  //get age (between 1 and 130)
 
   //calculate ticket price
 
